df79306dc604778bd446ec933518b014dfd40dc5
Rafał Szewczyk <admin@rafsze.pl>
2017-07-23 22:08:22 +0200

    Add SneakyThrow class to API

---
+++ API/src/main/java/pl/north93/zgame/api/global/utils/SneakyThrow.java
@@ -0,0 +1,19 @@
+package pl.north93.zgame.api.global.utils;
+
+public class SneakyThrow
+{
+    private SneakyThrow()
+    {
+    }
+    
+    public static void sneaky(Throwable e)
+    {
+        SneakyThrow.<RuntimeException>sneaky0(e);
+    }
+    
+    @SuppressWarnings("unchecked")
+    public static <T extends Throwable> void sneaky0(Throwable e) throws T
+    {
+        throw (T) e;
+    }
+}
