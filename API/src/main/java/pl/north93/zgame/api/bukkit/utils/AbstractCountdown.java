72fcd5205a7f58f200cffae942fb313dfea32de9
Michał Kasprzyk <northpl93@gmail.com>
2017-08-27 20:13:59 +0200

    Revert change in SimpleCountdown. Static field injecting still doesn't work well outside component package.

--- API/src/main/java/pl/north93/zgame/api/bukkit/utils/AbstractCountdown.java
+++ API/src/main/java/pl/north93/zgame/api/bukkit/utils/AbstractCountdown.java
@@ -8,12 +8,12 @@ import org.bukkit.scheduler.BukkitRunnable;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
-import pl.north93.zgame.api.global.component.annotations.bean.Inject;
+import pl.north93.zgame.api.bukkit.BukkitApiCore;
+import pl.north93.zgame.api.global.API;
 
 public abstract class AbstractCountdown extends BukkitRunnable
 {
-    @Inject
-    private static JavaPlugin PLUGIN;
+    private static JavaPlugin PLUGIN = ((BukkitApiCore) API.getApiCore()).getPluginMain();
     private int time;
 
     public AbstractCountdown(final int time)
