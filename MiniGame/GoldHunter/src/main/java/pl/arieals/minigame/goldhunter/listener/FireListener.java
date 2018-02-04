884bdc5a566a770e6c8aaf8c33422e6c73dafad3
IfarPL <admin@rafsze.pl>
2018-02-04 16:54:23 +0100

    [GH] Fire should burn wood planks now

--- MiniGame/GoldHunter/src/main/java/pl/arieals/minigame/goldhunter/listener/FireListener.java
+++ MiniGame/GoldHunter/src/main/java/pl/arieals/minigame/goldhunter/listener/FireListener.java
@@ -1,19 +1,31 @@
 package pl.arieals.minigame.goldhunter.listener;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.BlockBurnEvent;
 import org.bukkit.event.block.BlockIgniteEvent;
 import org.bukkit.event.block.BlockIgniteEvent.IgniteCause;
 
+import pl.arieals.minigame.goldhunter.GoldHunter;
 import pl.north93.zgame.api.bukkit.utils.AutoListener;
+import pl.north93.zgame.api.global.component.annotations.bean.Inject;
 
 public class FireListener implements AutoListener
 {
+    private static final List<Material> BURNABLE_MATERIALS = Arrays.asList(Material.WOOD, Material.WOOD_STEP, Material.WOOD_DOUBLE_STEP);
+    
+    @Inject
+    private static GoldHunter goldHunter;
+    
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onFireSpread(BlockIgniteEvent event)
     {
-        if ( event.getCause() == IgniteCause.LAVA || event.getCause() == IgniteCause.SPREAD )
+        if ( !goldHunter.isGameWorld(event.getBlock().getWorld()) || event.getCause() == IgniteCause.LAVA 
+                || event.getCause() == IgniteCause.SPREAD )
         {
             event.setCancelled(true);
         }
@@ -22,6 +34,10 @@ public class FireListener implements AutoListener
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onBlockBurn(BlockBurnEvent event)
     {
-        event.setCancelled(true);
+        if ( !goldHunter.isGameWorld(event.getBlock().getWorld()) 
+                || !BURNABLE_MATERIALS.contains(event.getBlock().getType()) )
+        {
+            event.setCancelled(true);
+        }
     }
 }
