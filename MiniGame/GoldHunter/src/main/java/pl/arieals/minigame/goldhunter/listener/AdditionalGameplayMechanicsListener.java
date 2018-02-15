5dc6201bdcfdd0902dafb8c432042e0730a1ff88
IfarPL <admin@rafsze.pl>
2018-02-15 23:15:52 +0100

    [GH] Move contents of BlockDropListener to AdditionalGameplayMechanicsListener

--- MiniGame/GoldHunter/src/main/java/pl/arieals/minigame/goldhunter/listener/AdditionalGameplayMechanicsListener.java
+++ MiniGame/GoldHunter/src/main/java/pl/arieals/minigame/goldhunter/listener/AdditionalGameplayMechanicsListener.java
@@ -1,9 +1,13 @@
 package pl.arieals.minigame.goldhunter.listener;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.event.player.PlayerFishEvent.State;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
 
 import pl.arieals.minigame.goldhunter.GoldHunter;
@@ -34,6 +38,23 @@ public class AdditionalGameplayMechanicsListener implements AutoListener
         {
             Vector toCatcher = player.getPlayer().getLocation().subtract(caught.getLocation()).toVector();
             caught.setVelocity(toCatcher.normalize().setY(0).multiply(0.7454).setY(0.569));
+        }
+    }
+    
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void everySolidBlockDropPlanks(BlockBreakEvent event)
+    {
+        GoldHunterPlayer player = goldHunter.getPlayer(event.getPlayer());
+        
+        if ( player != null )
+        {
+            event.setExpToDrop(0);
+            event.setDropItems(false);
+            
+            if ( event.getBlock().getType().isSolid() )
+            {
+                event.getBlock().getWorld().dropItem(event.getBlock().getLocation().add(0.5, 0.5, 0.5), new ItemStack(Material.WOOD));
+            }
         }
     }
 }
