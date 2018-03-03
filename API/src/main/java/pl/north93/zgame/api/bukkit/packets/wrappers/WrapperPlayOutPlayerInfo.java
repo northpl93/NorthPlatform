3dbdfc983c4db1a0ba93d64810c7bfd77f819334
IfarPL <admin@rafsze.pl>
2018-03-03 22:32:18 +0100

    Add PlayerInfoData wrapper to WrappedPlayOutPlayerInfo

--- API/src/main/java/pl/north93/zgame/api/bukkit/packets/wrappers/WrapperPlayOutPlayerInfo.java
+++ API/src/main/java/pl/north93/zgame/api/bukkit/packets/wrappers/WrapperPlayOutPlayerInfo.java
@@ -1,44 +1,24 @@
 package pl.north93.zgame.api.bukkit.packets.wrappers;
 
-import static java.lang.invoke.MethodType.methodType;
-
-
 import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodType;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import com.mojang.authlib.GameProfile;
 
 import net.minecraft.server.v1_12_R1.EnumGamemode;
 import net.minecraft.server.v1_12_R1.IChatBaseComponent;
 import net.minecraft.server.v1_12_R1.PacketPlayOutPlayerInfo;
 import net.minecraft.server.v1_12_R1.PacketPlayOutPlayerInfo.EnumPlayerInfoAction;
+import pl.north93.zgame.api.global.utils.lang.SneakyThrow;
 
-import com.mojang.authlib.GameProfile;
-
-/*
- * Stosujemy tu troche magii poniewaz uposledzony kod minecrafta
- * buguje kompilator javy i nie da sie bezposrednio uzyc klasy
- * PlayerInfoData. Dlatego recznie zdobywam jej konstruktor
- * przez MethodHandle i uzywam go w #addPlayerData.
- */
 public class WrapperPlayOutPlayerInfo extends AbstractWrapper<PacketPlayOutPlayerInfo>
 {
     private static final MethodHandle get_field_action = unreflectGetter(PacketPlayOutPlayerInfo.class, "a");
     private static final MethodHandle set_field_action = unreflectSetter(PacketPlayOutPlayerInfo.class, "a");
     private static final MethodHandle get_field_data = unreflectGetter(PacketPlayOutPlayerInfo.class, "b");
-    private static final MethodHandle data_constructor;
-
-    static
-    {
-        try
-        {
-            final Class<?> clazz = Class.forName("net.minecraft.server.v1_12_R1.PacketPlayOutPlayerInfo$PlayerInfoData");
-            data_constructor = lookup.findConstructor(clazz, methodType(void.class, PacketPlayOutPlayerInfo.class, GameProfile.class, int.class, EnumGamemode.class, IChatBaseComponent.class))
-                                     .asType(methodType(Object.class, PacketPlayOutPlayerInfo.class, GameProfile.class, int.class, EnumGamemode.class, IChatBaseComponent.class));
-        }
-        catch (final Throwable throwable)
-        {
-            throw new RuntimeException(throwable);
-        }
-    }
 
     public WrapperPlayOutPlayerInfo(final PacketPlayOutPlayerInfo packet)
     {
@@ -69,17 +49,85 @@ public class WrapperPlayOutPlayerInfo extends AbstractWrapper<PacketPlayOutPlaye
         }
     }
 
-    @SuppressWarnings("unchecked")
+    @Deprecated
     public void addPlayerData(final GameProfile gameProfile, final int ping, final EnumGamemode gameMode, final IChatBaseComponent displayName)
     {
-        try
+        addPlayerData(new PlayerInfoData(gameProfile, ping, gameMode, displayName));
+    }
+    
+    public List<Object> getRawData()
+    {
+        return SneakyThrow.sneaky(() -> (List<Object>) get_field_data.invokeExact(packet));
+    }
+    
+    public List<PlayerInfoData> getPlayerData()
+    {
+        return getRawData().stream().map(PlayerInfoData::new).collect(Collectors.toCollection(ArrayList::new));
+    }
+    
+    public void addPlayerData(PlayerInfoData data)
+    {
+        getRawData().add(data.handle);
+    }
+    
+    public static class PlayerInfoData
+    {
+        private static final MethodHandle GET_GAMEPROFILE;
+        private static final MethodHandle GET_PING;
+        private static final MethodHandle GET_GAMEMODE;
+        private static final MethodHandle GET_DISPLAY_NAME;
+        
+        private static final MethodHandle CONSTRUCT;
+        
+        private final Object handle; // We cannot use direct reference due to badly built spigot jar which causes compile error when we use PlayerInfoData class.
+        
+        static
         {
-            final List dataList = (List) get_field_data.invokeExact(this.packet);
-            dataList.add(data_constructor.invokeExact(this.packet, gameProfile, ping, gameMode, displayName));
+            try
+            {
+                Class<?> clazz = Class.forName("net.minecraft.server.v1_12_R1.PacketPlayOutPlayerInfo$PlayerInfoData");
+                CONSTRUCT = lookup.findConstructor(clazz, MethodType.methodType(void.class, PacketPlayOutPlayerInfo.class, GameProfile.class, int.class, EnumGamemode.class, IChatBaseComponent.class));
+            
+                GET_GAMEPROFILE = unreflectGetter(clazz, "d");
+                GET_PING = unreflectGetter(clazz, "b");
+                GET_GAMEMODE = unreflectGetter(clazz, "c");
+                GET_DISPLAY_NAME = unreflectGetter(clazz, "e");
+            }
+            catch ( Throwable e ) 
+            {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
         }
-        catch (final Throwable throwable)
+        
+        public PlayerInfoData(Object handle)
         {
-            throw new RuntimeException(throwable);
+            this.handle = handle;
+        }
+        
+        public PlayerInfoData(GameProfile gameProfile, int ping, EnumGamemode gamemode, IChatBaseComponent displayName)
+        {
+            this.handle = SneakyThrow.sneaky(() -> CONSTRUCT.invoke(null, gameProfile, ping, gamemode, displayName)); // It seems that we don't have to pass outer class refence
+        }
+        
+        public GameProfile getGameProfile()
+        {
+            return SneakyThrow.sneaky(() -> (GameProfile) GET_GAMEPROFILE.invoke(handle));
+        }
+        
+        public int getPing()
+        {
+            return SneakyThrow.sneaky(() -> (Integer) GET_PING.invoke(handle)).intValue();
+        }
+        
+        public EnumGamemode getGameMode()
+        {
+            return SneakyThrow.sneaky(() -> (EnumGamemode) GET_GAMEMODE.invoke(handle));
+        }
+        
+        public IChatBaseComponent getDisplayName()
+        {
+            return SneakyThrow.sneaky(() -> (IChatBaseComponent) GET_DISPLAY_NAME.invoke(handle));
         }
     }
 }
