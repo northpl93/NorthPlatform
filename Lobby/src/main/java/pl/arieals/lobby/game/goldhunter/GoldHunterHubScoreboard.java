4767e3f309ed6cbeb4394da3abcb9c7f2caacb73
Michał Kasprzyk <northpl93@gmail.com>
2018-06-12 21:56:12 +0200

    Implement base GoldHunter scoreboard.

--- Lobby/src/main/java/pl/arieals/lobby/game/goldhunter/GoldHunterHubScoreboard.java
+++ Lobby/src/main/java/pl/arieals/lobby/game/goldhunter/GoldHunterHubScoreboard.java
@@ -2,26 +2,61 @@ package pl.arieals.lobby.game.goldhunter;
 
 import java.util.List;
 
+import pl.arieals.api.minigame.shared.api.statistics.IStatisticHolder;
+import pl.arieals.api.minigame.shared.api.statistics.IStatisticsManager;
+import pl.arieals.api.minigame.shared.api.statistics.type.HigherNumberBetterStatistic;
 import pl.arieals.lobby.game.HubScoreboardLayout;
+import pl.north93.zgame.api.bukkit.scoreboard.ContentBuilder;
 import pl.north93.zgame.api.bukkit.scoreboard.IScoreboardContext;
+import pl.north93.zgame.api.bukkit.scoreboard.IScoreboardLayout;
+import pl.north93.zgame.api.global.component.annotations.bean.Inject;
+import pl.north93.zgame.api.global.messages.Messages;
+import pl.north93.zgame.api.global.messages.MessagesBox;
 
 public class GoldHunterHubScoreboard extends HubScoreboardLayout
 {
+    @Inject @Messages("HubGoldHunter")
+    private MessagesBox        messages;
+    @Inject
+    private IStatisticsManager statisticsManager;
+
     @Override
     public void initContext(final IScoreboardContext context)
     {
+        final IStatisticHolder playerHolder = this.statisticsManager.getPlayerHolder(context.getPlayer().getUniqueId());
 
+        final HigherNumberBetterStatistic killsStat = new HigherNumberBetterStatistic("goldhunter/kills");
+        context.setCompletableFuture("kills", playerHolder.getBest(killsStat));
+
+        final HigherNumberBetterStatistic winsStat = new HigherNumberBetterStatistic("goldhunter/wins");
+        context.setCompletableFuture("wins", playerHolder.getBest(winsStat));
     }
 
     @Override
     public String getTitle(final IScoreboardContext context)
     {
-        return null;
+        return this.messages.getMessage(context.getLocale(), "scoreboard.title");
     }
 
     @Override
     public List<String> getContent(final IScoreboardContext context)
     {
-        return null;
+        final ContentBuilder builder = IScoreboardLayout.builder();
+
+        builder.box(this.messages).locale(context.getLocale());
+        builder.add("");
+
+        builder.translated("scoreboard.kills",  this.parseNumber(context.getCompletableFuture("kills")));
+        builder.add("");
+
+        builder.translated("scoreboard.wins", this.parseNumber(context.getCompletableFuture("wins")));
+        builder.add("");
+
+        builder.translated("scoreboard.money", this.getPlayerCurrency(context.getPlayer()));
+        builder.add("");
+
+        builder.translated("scoreboard.ip");
+
+        return builder.getContent();
     }
 }
