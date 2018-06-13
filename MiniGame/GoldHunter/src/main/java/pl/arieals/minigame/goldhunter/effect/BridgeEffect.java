1d3151d16cd2302f9f115fc4eb17667bad4995f9
Rafał Szewczyk <admin@rafsze.pl>
2018-06-13 23:33:41 +0200

    [GH] Players cannot jump when they're building bridge now

--- MiniGame/GoldHunter/src/main/java/pl/arieals/minigame/goldhunter/effect/BridgeEffect.java
+++ MiniGame/GoldHunter/src/main/java/pl/arieals/minigame/goldhunter/effect/BridgeEffect.java
@@ -1,5 +1,8 @@
 package pl.arieals.minigame.goldhunter.effect;
 
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
 import pl.arieals.minigame.goldhunter.player.Effect;
 
 public class BridgeEffect extends Effect
@@ -12,11 +15,13 @@ public class BridgeEffect extends Effect
     protected void onStart()
     {
         getPlayer().setBuildBridgeActive(true);
+        getPlayer().getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.JUMP, Integer.MAX_VALUE, 128));
     }
     
     @Override
     protected void onEnd()
     {
         getPlayer().setBuildBridgeActive(false);
+        getPlayer().getPlayer().removePotionEffect(PotionEffectType.JUMP);
     }
 }
