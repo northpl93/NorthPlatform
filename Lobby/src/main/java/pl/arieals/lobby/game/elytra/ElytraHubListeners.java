b5ff200808e3f2674233fcaa92e24464a0cc6f9c
Michał Kasprzyk <northpl93@gmail.com>
2018-06-24 18:27:39 +0200

    Update hub hotbar in ElytraHubListeners.

--- Lobby/src/main/java/pl/arieals/lobby/game/elytra/ElytraHubListeners.java
+++ Lobby/src/main/java/pl/arieals/lobby/game/elytra/ElytraHubListeners.java
@@ -5,6 +5,7 @@ import org.bukkit.event.EventHandler;
 import pl.arieals.api.minigame.server.lobby.hub.event.PlayerSwitchedHubEvent;
 import pl.arieals.api.minigame.server.lobby.hub.HubWorld;
 import pl.arieals.lobby.game.HubListener;
+import pl.arieals.lobby.ui.HubHotbar;
 import pl.north93.zgame.api.bukkit.scoreboard.IScoreboardManager;
 import pl.north93.zgame.api.global.component.annotations.bean.Inject;
 
@@ -22,6 +23,8 @@ public class ElytraHubListeners extends HubListener
         }
 
         this.scoreboardManager.setLayout(event.getPlayer(), new ElytraHubScoreboard());
+
+        new HubHotbar().display(event.getPlayer());
     }
 
     @Override
