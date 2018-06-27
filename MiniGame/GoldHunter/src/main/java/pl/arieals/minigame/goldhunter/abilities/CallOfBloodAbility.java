33ebf4b0d5286a9f46edc906a5bc41f98a04cb72
Rafał Szewczyk <admin@rafsze.pl>
2018-06-27 13:14:33 +0200

    [GH] New version of call of blood ability

--- MiniGame/GoldHunter/src/main/java/pl/arieals/minigame/goldhunter/abilities/CallOfBloodAbility.java
+++ MiniGame/GoldHunter/src/main/java/pl/arieals/minigame/goldhunter/abilities/CallOfBloodAbility.java
@@ -1,20 +1,40 @@
 package pl.arieals.minigame.goldhunter.abilities;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageEvent;
+
+import pl.arieals.minigame.goldhunter.GoldHunter;
 import pl.arieals.minigame.goldhunter.effect.CallOfBloodEffect;
 import pl.arieals.minigame.goldhunter.player.AbilityHandler;
 import pl.arieals.minigame.goldhunter.player.GoldHunterPlayer;
+import pl.north93.zgame.api.global.component.annotations.bean.Inject;
 
 public class CallOfBloodAbility implements AbilityHandler
 {
-    public static final int EFFECT_DURATION = 400;
-    public static final double BLOOD_TRIBUTE = 8;
+    public static final int BASE_DURATION = 60;
+    public static final int PER_LEVEL = 10;
+    public static final double BLOOD_TRIBUTE = 5;
+    
+    @Inject
+    public static GoldHunter goldHunter;
     
     @Override
     public boolean onUse(GoldHunterPlayer player)
     {
         player.getAbilityTracker().suspendAbilityLoading();
-        player.getEffectTracker().addEffect(new CallOfBloodEffect(), EFFECT_DURATION).onComplete(player.getAbilityTracker()::resetAbilityLoading);
+        int duration = BASE_DURATION + player.getShopItemLevel("berserker.abilityduration") * PER_LEVEL;
+        player.getEffectTracker().addEffect(new CallOfBloodEffect(), duration).onComplete(player.getAbilityTracker()::resetAbilityLoading);
         player.getPlayer().damage(BLOOD_TRIBUTE);
         return true;
+    }
+    
+    @EventHandler
+    public void onDamage(EntityDamageEvent event)
+    {
+        //GoldHunterPlayer player = goldHunter.getPlayer(event.getEntity());
+        //if ( player.getEffectTracker().hasEffectOfType(CallOfBloodEffect.class) )
+        //{
+        //    event.setCancelled(true);
+        //}
     }
 }
--- MiniGame/GoldHunter/src/main/java/pl/arieals/minigame/goldhunter/effect/CallOfBloodEffect.java
+++ MiniGame/GoldHunter/src/main/java/pl/arieals/minigame/goldhunter/effect/CallOfBloodEffect.java
@@ -1,16 +1,9 @@
 package pl.arieals.minigame.goldhunter.effect;
 
-import java.util.Arrays;
-
-import org.bukkit.enchantments.Enchantment;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 import pl.arieals.minigame.goldhunter.player.Effect;
-import pl.north93.zgame.api.bukkit.utils.itemstack.MaterialUtils;
 
 public class CallOfBloodEffect extends Effect
 {
@@ -23,43 +16,15 @@ public class CallOfBloodEffect extends Effect
     protected void onStart()
     {
         getPlayer().getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 0, false, false));
-        addEnchantsToSwords();
+        getPlayer().getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, Integer.MAX_VALUE, 1, false, false));
+        getPlayer().getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, Integer.MAX_VALUE, 0, false, false));
     }
     
     @Override
     protected void onEnd()
     {
         getPlayer().getPlayer().removePotionEffect(PotionEffectType.SPEED);
-        removeEnchantsFromSwords();
-    }
-    
-    private void addEnchantsToSwords()
-    {
-        PlayerInventory inv = getPlayer().getPlayer().getInventory();
-        
-        Arrays.stream(inv.getContents()).filter(is -> is != null && MaterialUtils.isSword(is.getType())).forEach(this::applyEnchant);
-        getPlayer().getPlayer().updateInventory();
-    }
-    
-    private void applyEnchant(ItemStack is)
-    {
-        ItemMeta meta = is.getItemMeta();
-        meta.addEnchant(Enchantment.DAMAGE_ALL, 1, true);
-        is.setItemMeta(meta);
-    }
-    
-    private void removeEnchantsFromSwords()
-    {
-        PlayerInventory inv = getPlayer().getPlayer().getInventory();
-        
-        Arrays.stream(inv.getContents()).filter(is -> is != null && MaterialUtils.isSword(is.getType())).forEach(this::removeEnchant);
-        getPlayer().getPlayer().updateInventory();
-    }
-    
-    private void removeEnchant(ItemStack is)
-    {
-        ItemMeta meta = is.getItemMeta();
-        meta.removeEnchant(Enchantment.DAMAGE_ALL);
-        is.setItemMeta(meta);
+        getPlayer().getPlayer().removePotionEffect(PotionEffectType.DAMAGE_RESISTANCE);
+        getPlayer().getPlayer().removePotionEffect(PotionEffectType.INCREASE_DAMAGE);
     }
 }
