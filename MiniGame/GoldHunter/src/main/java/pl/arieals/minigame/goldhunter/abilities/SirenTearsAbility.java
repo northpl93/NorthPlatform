1022e77c7abedd9714a42b394d278ee153bcac1b
Rafał Szewczyk <admin@rafsze.pl>
2018-06-27 13:30:15 +0200

    [GH] Update healer ability properties

--- MiniGame/GoldHunter/src/main/java/pl/arieals/minigame/goldhunter/abilities/SirenTearsAbility.java
+++ MiniGame/GoldHunter/src/main/java/pl/arieals/minigame/goldhunter/abilities/SirenTearsAbility.java
@@ -6,11 +6,15 @@ import pl.arieals.minigame.goldhunter.player.GoldHunterPlayer;
 
 public class SirenTearsAbility implements AbilityHandler
 {
+    private static final int BASE_DURATION = 100;
+    private static final int PER_LEVEL = 10;
+    
     @Override
     public boolean onUse(GoldHunterPlayer player)
     {
         player.getAbilityTracker().suspendAbilityLoading();
-        player.getEffectTracker().addEffect(new SirenTearsEffect(10), 320).onComplete(player.getAbilityTracker()::resetAbilityLoading);
+        int duration = BASE_DURATION + player.getShopItemLevel("medic.abilityduration") * PER_LEVEL;
+        player.getEffectTracker().addEffect(new SirenTearsEffect(10), duration).onComplete(player.getAbilityTracker()::resetAbilityLoading);
         return true;
     }
 }
--- MiniGame/GoldHunter/src/main/java/pl/arieals/minigame/goldhunter/effect/HealingEffect.java
+++ MiniGame/GoldHunter/src/main/java/pl/arieals/minigame/goldhunter/effect/HealingEffect.java
@@ -11,12 +11,16 @@ public class HealingEffect extends Effect
         setBarColor(EffectBarColor.GREEN);
     }
     
+    // TODO: make this effect more general 
+    
     @Tick
     private void heal()
     {
         double health = getPlayer().getPlayer().getHealth();
         double maxHealth = getPlayer().getPlayer().getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue();
         
-        getPlayer().getPlayer().setHealth(Math.min(maxHealth, health + 0.025));
+        getPlayer().getPlayer().setHealth(Math.min(maxHealth, health + 0.1));
+        
+        getPlayer().getMinecraftPlayer().setAbsorptionHearts(Math.min(6, getPlayer().getMinecraftPlayer().getAbsorptionHearts() + 0.1f));
     }
 }
