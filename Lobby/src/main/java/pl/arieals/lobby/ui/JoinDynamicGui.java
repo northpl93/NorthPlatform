beeb37992805d68e41227b997134c5cfba8de86c
Rafał Szewczyk <admin@rafsze.pl>
2018-07-02 23:35:22 +0200

    [GH] Fix behavior of dynamic play gui

--- Lobby/src/main/java/pl/arieals/lobby/ui/JoinDynamicGui.java
+++ Lobby/src/main/java/pl/arieals/lobby/ui/JoinDynamicGui.java
@@ -17,12 +17,14 @@ import pl.arieals.api.minigame.server.lobby.arenas.ArenaQuery;
 import pl.arieals.api.minigame.server.lobby.arenas.IArenaClient;
 import pl.arieals.api.minigame.shared.api.GameIdentity;
 import pl.arieals.api.minigame.shared.api.arena.IArena;
+import pl.arieals.api.minigame.shared.api.arena.StandardArenaMetaData;
 import pl.arieals.lobby.play.PlayGameController;
 import pl.north93.zgame.api.bukkit.gui.ConfigGuiIcon;
 import pl.north93.zgame.api.bukkit.gui.Gui;
 import pl.north93.zgame.api.bukkit.gui.IGuiIcon;
 import pl.north93.zgame.api.bukkit.gui.element.dynamic.DynamicElementData;
 import pl.north93.zgame.api.bukkit.gui.element.dynamic.DynamicRenderer;
+import pl.north93.zgame.api.bukkit.gui.event.GuiClickEvent;
 import pl.north93.zgame.api.bukkit.utils.itemstack.ItemStackBuilder;
 import pl.north93.zgame.api.global.commands.Arguments;
 import pl.north93.zgame.api.global.commands.NorthCommandSender;
@@ -31,14 +33,17 @@ import pl.north93.zgame.api.global.component.annotations.bean.Inject;
 import pl.north93.zgame.api.global.messages.Messages;
 import pl.north93.zgame.api.global.messages.MessagesBox;
 import pl.north93.zgame.api.global.messages.TranslatableString;
-import pl.north93.zgame.api.global.metadata.MetaKey;
 import pl.north93.zgame.api.global.utils.Vars;
 
 public class JoinDynamicGui extends Gui
 {
     private static final Comparator<IArena> ARENA_COMPARATOR = Comparator.comparingInt(IArena::getPlayersCount).reversed();
     
-    @Inject @Messages("UserInterface")
+    private static final double ALMOST_FULL_RATIO = 0.65;
+    private static final double OVERFULL_RATIO = 1.25;
+    
+    @Inject
+    @Messages("UserInterface")
     private static MessagesBox        messages;
     @Inject
     private static PlayGameController playController;
@@ -81,11 +86,7 @@ public class JoinDynamicGui extends Gui
         }
 
         arenas.stream().filter(this::isFull).sorted(ARENA_COMPARATOR).forEach(arena -> result.add(createElementForArena(arena)));
-        
-        if ( viewAll )
-        {
-            arenas.stream().filter(this::isOverFull).sorted(ARENA_COMPARATOR).forEach(arena -> result.add(createElementForArena(arena)));
-        }
+        arenas.stream().filter(this::isOverFull).sorted(ARENA_COMPARATOR).forEach(arena -> result.add(createElementForArena(arena)));
         
         if ( result.size() == 0 )
         {
@@ -100,12 +101,21 @@ public class JoinDynamicGui extends Gui
         ItemStack is = new ItemStackBuilder().material(isEmpty(arena) || isAlmostEmpty(arena) || isAlmostFull(arena) ? Material.CHEST : Material.ENDER_CHEST)
                 .amount(Math.max(1, getSignedPlayers(arena))).build();
         
-        IGuiIcon icon = ConfigGuiIcon.builder().itemStack(is).name(TranslatableString.of(messages, "@play.dynamic.icon.name"))
-                .lore(TranslatableString.of(messages, "@play.dynamic.icon.lore$players,max,map"))
+        IGuiIcon icon = ConfigGuiIcon.builder().itemStack(is)
+                .name(TranslatableString.of(messages, isOverFull(arena) ? "@play.dynamic.icon.full.name" : "@play.dynamic.icon.join.name"))
+                .lore(TranslatableString.of(messages, isOverFull(arena) ? "@play.dynamic.icon.full.lore$players,max,map" : "@play.dynamic.icon.join.lore$players,max,map"))
                 .build();
                 
-        return DynamicElementData.builder().icon(icon).clickHandler(event -> playController.playGame(event.getWhoClicked(), arena))
+        return DynamicElementData.builder().icon(icon).clickHandler(event -> tryJoin(event.getWhoClicked(), arena))
                 .vars(Vars.builder().and("players", getSignedPlayers(arena)).and("max", arena.getMaxPlayers()).and("map", arena.getWorldDisplayName()).build()).build();
+    }
+    
+    private void tryJoin(Player player, IArena arena)
+    {
+        if ( viewAll || !isOverFull(arena) )
+        {
+            playController.playGame(player, arena);
+        }
     }
     
     private DynamicElementData createNonArenaElement()
@@ -129,29 +139,29 @@ public class JoinDynamicGui extends Gui
     
     private boolean isAlmostEmpty(IArena arena)
     {
-        return getSignedPlayers(arena) > 0 && getSignedPlayers(arena) < DioriteMathUtils.ceil(arena.getMaxPlayers() * 0.65);
+        return getSignedPlayers(arena) > 0 && getSignedPlayers(arena) < DioriteMathUtils.ceil(arena.getMaxPlayers() * ALMOST_FULL_RATIO);
     }
     
     private boolean isAlmostFull(IArena arena)
     {
         int signedPlayers = getSignedPlayers(arena);
-        return signedPlayers >= DioriteMathUtils.ceil(arena.getMaxPlayers() * 0.65) && signedPlayers < arena.getMaxPlayers();
+        return signedPlayers >= DioriteMathUtils.ceil(arena.getMaxPlayers() * ALMOST_FULL_RATIO) && signedPlayers < arena.getMaxPlayers();
     }
     
     private boolean isFull(IArena arena)
     {
         int signedPlayers = getSignedPlayers(arena);
-        return signedPlayers >= arena.getMaxPlayers() && signedPlayers < DioriteMathUtils.ceil(arena.getMaxPlayers() * 1.34);
+        return signedPlayers >= arena.getMaxPlayers() && signedPlayers < DioriteMathUtils.ceil(arena.getMaxPlayers() * OVERFULL_RATIO);
     }
     
     private boolean isOverFull(IArena arena)
     {
-        return getSignedPlayers(arena) >= DioriteMathUtils.ceil(arena.getMaxPlayers() * 1.34);
+        return getSignedPlayers(arena) >= DioriteMathUtils.ceil(arena.getMaxPlayers() * OVERFULL_RATIO);
     }
     
     private int getSignedPlayers(IArena arena)
     {
-        Integer signedPlayers = arena.getMetadata().get(MetaKey.get("signedPlayers"));
+        Integer signedPlayers = arena.getMetadata().get(StandardArenaMetaData.SIGNED_PLAYERS);
         return signedPlayers != null ? signedPlayers.intValue() : arena.getPlayersCount();
     }
     
