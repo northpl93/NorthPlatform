70dd52b02599ff9e0c9406007a102eada87e9e94
Michał Kasprzyk <northpl93@gmail.com>
2018-07-05 00:29:25 +0200

    Implement way to set timed groups by command.

--- API/BaseFeatures/src/main/java/pl/north93/zgame/features/global/network/GroupsCommand.java
+++ API/BaseFeatures/src/main/java/pl/north93/zgame/features/global/network/GroupsCommand.java
@@ -1,5 +1,12 @@
 package pl.north93.zgame.features.global.network;
 
+import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
@@ -13,6 +20,7 @@ import pl.north93.zgame.api.global.messages.MessagesBox;
 import pl.north93.zgame.api.global.network.INetworkManager;
 import pl.north93.zgame.api.global.permissions.Group;
 import pl.north93.zgame.api.global.permissions.PermissionsManager;
+import pl.north93.zgame.api.global.utils.DateUtil;
 
 public class GroupsCommand extends NorthCommand
 {
@@ -37,19 +45,21 @@ public class GroupsCommand extends NorthCommand
     {
         if (args.length() == 0)
         {
-            sender.sendMessage("&e/" + label + " <gracz> - wyświetla grupę gracza");
-            sender.sendMessage("&e/" + label + " <gracz> <grupa> - zmienia grupę gracza");
+            sender.sendMessage("&e/" + label + " <gracz> - wyswietla grupe gracza");
+            sender.sendMessage("&e/" + label + " <gracz> <grupa> [czas] - zmienia grupe gracza");
         }
         else if (args.length() == 1)
         {
             final String username = args.asString(0);
 
-            boolean result = this.networkManager.getPlayers().access(username, online ->
+            final boolean result = this.networkManager.getPlayers().access(username, online ->
             {
-                sender.sendMessage("&eGrupa " + online.getNick() + " to " + online.getGroup().getName());
+                sender.sendMessage("&aGrupa &e{0} &ato &e{1}", online.getNick(), online.getGroup().getName());
+                this.sendExpirationInfo(sender, online.getGroupExpireAt());
             }, offline ->
             {
-                sender.sendMessage("&eGrupa " + offline.getLatestNick() + " (" + offline.getUuid() + ") to " + offline.getGroup().getName());
+                sender.sendMessage("&aGrupa &e{0} &a(&e{1}&a) to &e{2}", offline.getLatestNick(), offline.getUuid(), offline.getGroup().getName());
+                this.sendExpirationInfo(sender, offline.getGroupExpireAt());
             });
 
             if (! result)
@@ -60,28 +70,69 @@ public class GroupsCommand extends NorthCommand
         else if (args.length() == 2)
         {
             final String username = args.asString(0);
-            final Group newGroup = this.permissionsManager.getGroupByName(args.asString(1));
-            if (newGroup == null)
+            final String newGroup = args.asString(1);
+
+            this.changeGroup(sender, username, newGroup, 0);
+        }
+        else if (args.length() == 3)
+        {
+            final String username = args.asString(0);
+            final String newGroup = args.asString(1);
+            final long groupExpireAt;
+
+            try
             {
-                sender.sendMessage("&cNie ma takiej grupy!");
+                groupExpireAt = DateUtil.parseDateDiff(args.asString(2), true);
+            }
+            catch (final Exception e)
+            {
+                sender.sendMessage("&cNiepoprawny format czasu. Przyklad: 30d5h59m59s");
                 return;
             }
-            if (this.networkManager.getPlayers().access(username, player ->
-            {
-                player.setGroup(newGroup);
-                player.setGroupExpireAt(0);
-            }))
-            {
-                sender.sendMessage("&aPomyślnie zmieniono grupę na " + newGroup.getName());
-            }
-            else
-            {
-                sender.sendMessage(this.messages, "command.no_player");
-            }
+
+            this.changeGroup(sender, username, newGroup, groupExpireAt);
         }
         else
         {
-            sender.sendMessage("&cZła ilość argumentów!");
+            sender.sendMessage("&cZla ilosc argumentów!");
+        }
+    }
+
+    private void changeGroup(final NorthCommandSender sender, final String username, final String groupName, final long groupExpireAt)
+    {
+        final Group newGroup = this.permissionsManager.getGroupByName(groupName);
+        if (newGroup == null)
+        {
+            sender.sendMessage("&cNie ma takiej grupy!");
+            return;
+        }
+        if (this.networkManager.getPlayers().access(username, player ->
+        {
+            player.setGroup(newGroup);
+            player.setGroupExpireAt(groupExpireAt);
+        }))
+        {
+            sender.sendMessage("&aPomyslnie zmieniono grupe na " + newGroup.getName());
+        }
+        else
+        {
+            sender.sendMessage(this.messages, "command.no_player");
+        }
+    }
+
+    private void sendExpirationInfo(final NorthCommandSender sender, final long groupExpireAt)
+    {
+        if (groupExpireAt == 0)
+        {
+            sender.sendMessage("&eNigdy &anie wygasa");
+        }
+        else
+        {
+            final DateTimeFormatter formatter = ISO_LOCAL_DATE_TIME.withZone(ZoneId.of("Europe/Warsaw"));
+            final Instant instant = Instant.ofEpochMilli(groupExpireAt);
+
+            final String timeIso = formatter.format(instant);
+            sender.sendMessage("&aWygasnie &e{0}", timeIso);
         }
     }
 
