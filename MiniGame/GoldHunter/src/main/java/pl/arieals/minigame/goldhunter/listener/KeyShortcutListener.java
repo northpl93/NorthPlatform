9b50185cc23380cc1da457e030930a56af97e482
Rafał Szewczyk <admin@rafsze.pl>
2018-09-14 23:52:55 +0200

    Revert "[GH] Try to fix 1.13.1 Q key behavior"
    This reverts commit 206c246dcfc54d143aaad1ea9388c98197d9343c.

--- MiniGame/GoldHunter/src/main/java/pl/arieals/minigame/goldhunter/listener/KeyShortcutListener.java
+++ MiniGame/GoldHunter/src/main/java/pl/arieals/minigame/goldhunter/listener/KeyShortcutListener.java
@@ -1,20 +1,14 @@
 package pl.arieals.minigame.goldhunter.listener;
 
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerSwapHandItemsEvent;
-
-import net.minecraft.server.v1_12_R1.EntityPlayer;
-import net.minecraft.server.v1_12_R1.ItemStack;
-import net.minecraft.server.v1_12_R1.PacketPlayOutSetSlot;
 
 import pl.arieals.minigame.goldhunter.GoldHunter;
 import pl.arieals.minigame.goldhunter.gui.JoinTeamGui;
 import pl.arieals.minigame.goldhunter.gui.SelectClassGui;
 import pl.arieals.minigame.goldhunter.player.GoldHunterPlayer;
 import pl.north93.northspigot.event.PlayerPressQEvent;
-import pl.north93.zgame.api.bukkit.player.INorthPlayer;
 import pl.north93.zgame.api.bukkit.utils.AutoListener;
 
 public class KeyShortcutListener implements AutoListener
@@ -40,18 +34,6 @@ public class KeyShortcutListener implements AutoListener
         {
             new JoinTeamGui(player).open();
         }
-        
-        // due to different 1.13.1 behavior we have to update clientside held item slot 
-        refreshSlot(event.getPlayer());
-    }
-    
-    private void refreshSlot(Player player)
-    {
-        EntityPlayer entityPlayer = INorthPlayer.asCraftPlayer(player).getHandle();
-        
-        int handSlot = entityPlayer.inventory.itemInHandIndex;
-        ItemStack itemInHand = entityPlayer.inventory.getItem(handSlot);
-        entityPlayer.playerConnection.sendPacket(new PacketPlayOutSetSlot(0, handSlot, itemInHand));
     }
     
     @EventHandler(priority = EventPriority.HIGHEST)
