01e6867ddb55cd7aabf4adeefb22efd0e7cdac0e
Michał Kasprzyk <northpl93@gmail.com>
2018-09-23 16:47:42 +0200

    Move skyblock modules to SkyBlock directory. Remove unifinished and unused SkyBlock.Quests. Reorganise modules section in platform pom.xml

--- SkyBlock.Quests/src/main/java/pl/arieals/skyblock/quests/shared/api/StatisticType.java
+++
@@ -1,7 +0,0 @@
-package pl.arieals.skyblock.quests.shared.api;
-
-public enum StatisticType
-{
-    MOB_KILL,
-    CRAFT_ITEM
-}
--- SkyBlock.Quests/src/main/java/pl/arieals/skyblock/quests/shared/impl/quest/QuestFactory.java
+++
@@ -1,8 +0,0 @@
-package pl.arieals.skyblock.quests.shared.impl.quest;
-
-public final class QuestFactory
-{
-    public static final QuestFactory INSTANCE = new QuestFactory();
-
-    // todo
-}
