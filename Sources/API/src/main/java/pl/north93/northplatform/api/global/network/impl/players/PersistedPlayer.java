2a339d30d88764bab9ddc371dc30034d304c8faf
Michał Kasprzyk <northpl93@gmail.com>
2020-10-04 01:31:50 +0200

    Add missing generic types in PersistedPlayer & reformat.

--- Sources/API/src/main/java/pl/north93/northplatform/api/global/network/impl/players/PersistedPlayer.java
+++ Sources/API/src/main/java/pl/north93/northplatform/api/global/network/impl/players/PersistedPlayer.java
@@ -1,19 +1,23 @@
 package pl.north93.northplatform.api.global.network.impl.players;
 
-import lombok.*;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import pl.north93.northplatform.api.global.metadata.MetaKey;
 import pl.north93.northplatform.api.global.network.players.IOfflinePlayer;
 import pl.north93.northplatform.api.global.network.players.IOnlinePlayer;
 import pl.north93.northplatform.api.global.network.players.IPlayer;
 import pl.north93.serializer.platform.annotations.NorthField;
 
-import java.time.Instant;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-
 /**
- * Reprezentuje encje gracza zapisaną w bazie danych MongoDB.
+ * Represents a player saved in the MongoDB database.
  */
 @Getter
 @Builder
@@ -22,16 +26,16 @@ import java.util.UUID;
 @AllArgsConstructor
 /*default*/ class PersistedPlayer
 {
-    private UUID    uuid;
+    private UUID uuid;
     private boolean premium;
-    private String  latestKnownUsername;
-    private String  displayName;
-    private String  group;
+    private String latestKnownUsername;
+    private String displayName;
+    private String group;
     private Instant groupExpireAt;
-    private boolean isSavedWhileOnline;
+    private boolean savedWhileOnline;
     private Instant savedAt;
     @NorthField(type = HashMap.class)
-    private Map     metadata;
+    private Map<String, Object> metadata;
 
     public static PersistedPlayer create(final IPlayer player)
     {
@@ -48,12 +52,12 @@ import java.util.UUID;
         {
             final IOnlinePlayer onlinePlayer = (IOnlinePlayer) player;
             builder.latestKnownUsername(onlinePlayer.getNick());
-            builder.isSavedWhileOnline(true);
+            builder.savedWhileOnline(true);
         }
         else if (player instanceof IOfflinePlayer)
         {
             builder.latestKnownUsername(player.getLatestNick());
-            builder.isSavedWhileOnline(false);
+            builder.savedWhileOnline(false);
         }
 
         final Map<String, Object> metadata = new HashMap<>();
